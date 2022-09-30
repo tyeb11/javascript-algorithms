e22f77eb77498f4a79271ec2daae10ba46e85461
tyeb11 <tayyeb.merchant@gmail.com>
2022-09-30 17:12:19 +0530

    linear-search

---
+++ src/algorithms/search/linear-search/index.js
@@ -0,0 +1,23 @@
+class LinearSearch {
+  constructor() {
+    this.arr = [3, 2, 8, 5, 1, 2, 7];
+  }
+  find(value) {
+    const foundIndexes = [];
+    this.arr.forEach((element, index) => {
+      if (element == value) {
+        foundIndexes.push(index);
+      }
+    });
+    return foundIndexes;
+  }
+  make(arr) {
+    if (Array.isArray(arr)) {
+      this.arr = arr;
+      return;
+    }
+    return "please provide array as a parameter";
+  }
+}
+
+var linearSearch = new LinearSearch();
