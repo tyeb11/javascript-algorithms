43cf5fcfd9107d7c56368338205bf1c4e3e63984
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-04 12:46:19 +0530

    stack

--- src/data-structures/stack/index.js
+++ src/data-structures/stack/index.js
@@ -8,34 +8,42 @@ class Node {
 class Stack {
   constructor() {
     this.top = null;
-    this.bottom = null;
     this.size = 0;
   }
   push(value) {
     let newNode = new Node(value);
-    if (this.bottom) {
+    if (this.top) {
       newNode.next = this.top;
       this.top = newNode;
     } else {
-      this.bottom = newNode;
       this.top = newNode;
     }
     this.size++;
     return this;
   }
   pop() {
-    if (this.top === this.bottom) {
-      this.top = null;
-      this.bottom = null;
-    } else {
-      this.top = this.top.next;
+    if (this.top === null) {
+      return this;
     }
+    this.top = this.top.next;
     this.size--;
+    return this;
+  }
+  reverce() {
+    let tmp = this.top;
+    let nextNode = null;
+    let prvNode = null;
+    for (let i = 0; i < this.size; i++) {
+      nextNode = tmp.next;
+      tmp.next = prvNode;
+      prvNode = tmp;
+      tmp = nextNode;
+    }
+    this.top = prvNode;
     return this;
   }
   display() {
     let tmp = this.top;
-
     while (tmp) {
       console.log(tmp.value);
       tmp = tmp.next;
